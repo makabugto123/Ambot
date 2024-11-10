1ea37d7ddf52804828913a3ae0328e8f493d359b
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-11-10 20:45:47 +0800

    Create photoleap.js

---
+++ commands/photoleap.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'photoleap',
+  description: 'Generate HD picture through your prompt,
+  usage: 'photoleap [prompt]',
+  author: 'coffee',
+  async execute(senderId, args, pageAccessToken) {
+    const prompt = args.join(' ');
+    if (!prompt) return sendMessage(senderId, { text: "Usage: photoleap dog with owner" }, pageAccessToken);
+
+    try {
+      const { data } = await axios.get(`https://joshweb.click/aigen?prompt=${encodeURIComponent(prompt)}`);
+      sendMessage(senderId, { text: data.result }, pageAccessToken);
+    } catch {
+      sendMessage(senderId, { text: 'Error generating response. Try again later.' }, pageAccessToken);
+    }
+  }
+};
