ce58f8c9d184a18f371da1b326d2963e20c51a10
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-11-10 17:12:27 +0800

    Update and rename encyclopedia.js to ency.js

--- commands/ency.js
+++ commands/ency.js
@@ -2,15 +2,15 @@ const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: 'encyclopedia',
+  name: 'ency',
   description: 'search for and retrieve detailed information from the Stanford Encyclopedia of Philosophy,
-  usage: 'encyclopedia [text]',
+  usage: 'ency [text]',
   author: 'coffee',
 
   async execute(senderId, args, pageAccessToken) {
 
     const prompt = args.join(' ');
-    if (!prompt) return sendMessage(senderId, { text: "Usage: encyclopedia <text>" }, pageAccessToken);
+    if (!prompt) return sendMessage(senderId, { text: "Usage: ency <text>" }, pageAccessToken);
 
     try {
        const { data } = await axios.get(`https://myapi-2f5b.onrender.com/stanford/${encodeURIComponent(prompt)}`);
