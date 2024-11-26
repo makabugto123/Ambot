ea57ab888aeef90562c8a6ef1ae132327dda1df3
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-11-26 21:48:53 +0800

    Update remini.js

--- commands/remini.js
+++ commands/remini.js
@@ -5,6 +5,7 @@ module.exports = {
   name: "remini",
   description: "enhance image quality",
   author: "developer",
+  usage: "Send any picture first then reply remini",
 
   async execute(senderId, args, pageAccessToken, imageUrl) {
     // Check if an image URL is provided
